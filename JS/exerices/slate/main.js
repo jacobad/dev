05ac9d6135f024b66ab66306ad10ad92a367c87a
jacobad <dizier91@gmail.com>
2019-01-18 17:35:25 +0100

    ardoise

---
+++ JS/exerices/slate/main.js
@@ -0,0 +1,8 @@
+'use strict';
+
+
+var canvas = document.getElementById('slate');
+
+var prog = new Program(canvas);
+
+//var program = new Program();
---
+++ JS/exerices/slate/pen.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var Pen = function()
+{
+    this.color = 'black';
+    this.size  = 1;
+};
+
+
+Pen.prototype.configure = function(context)
+{
+    context.strokeStyle = this.color;
+    context.lineWidth   = this.size;
+};
+
