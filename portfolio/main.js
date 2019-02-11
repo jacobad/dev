12da046a7fdd7811154705a8fdcb4ad8eb61eb6a
jacobad <dizier91@gmail.com>
2019-02-11 17:35:47 +0100

    essai 2

---
+++ portfolio/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+
+function qsj(){
+		if(document.getElementById('invisible').style.display == 'none'){
+    	document.getElementById('invisible').style.display = 'block';
+  		}
+  		else {
+    	document.getElementById('invisible').style.display = 'none';
+		}
+}
