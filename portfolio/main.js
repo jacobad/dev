0ad5943c0d6f22be496f957a80673c88787794d6
jacobad <dizier91@gmail.com>
2019-02-12 17:27:36 +0100

    avancer1

--- portfolio/main.js
+++ portfolio/main.js
@@ -1,5 +1,6 @@
 'use strict';
 
+/*Header clcik fonction*/
 
 function qsj(){
 		if(document.getElementById('invisible').style.display == 'none'){
@@ -7,5 +8,13 @@ function qsj(){
   		}
   		else {
     	document.getElementById('invisible').style.display = 'none';
+		}
+}
+function network(){
+	if(document.getElementById('invisibles').style.display == 'none'){
+    	document.getElementById('invisibles').style.display = 'block';
+  		}
+  		else {
+    	document.getElementById('invisibles').style.display = 'none';
 		}
 }
